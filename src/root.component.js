24a7a7fb3875144b04fae906672e8a01703493f2
Michael Belov <mikle0706@icloud.com>
2021-07-11 10:28:42 +0300

    add styled-component

--- src/root.component.js
+++ src/root.component.js
@@ -1,18 +1,43 @@
-// import style from "./navbar.module.scss";
+import styled from "styled-components";
 
-export default function Root(props) {
-  console.log();
+const Button = styled.button`
+  cursor: pointer;
+  background: blue;
+  outline: none;
+  border: none;
+  color: #fff;
+  padding: 10px 50px;
+  transition: all 0.3s ease;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+const NavBar = styled.section`
+  height: 60px;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 50px;
+`;
+
+const Root = (props) => {
   return (
     <>
-      <section>{props.name} is mounted! Vorseek app</section>
-      <button
-        type="button"
-        onClick={() => {
-          props.singleSpa.navigateToUrl("/app");
-        }}
-      >
-        Go to URL 'APP'
-      </button>
+      <NavBar>
+        <span>{props.name} Vorseek app &#169;</span>
+        <Button
+          type="button"
+          onClick={() => {
+            props.singleSpa.navigateToUrl("/app");
+          }}
+        >
+          Go to URL 'APP'
+        </Button>
+      </NavBar>
     </>
   );
-}
+};
+
+export default Root;
